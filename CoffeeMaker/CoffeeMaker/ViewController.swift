3d42a414481271a3773af8284dc36b4614e40a3f
therealniks <therealniks@gmail.com>
2021-04-21 21:46:43 +0300

    creating project

---
+++ CoffeeMaker/CoffeeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeMaker
+//
+//  Created by N!kS on 21.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
